e519066e7d52efa8915dcfe84839f516c6d7e28b
Frederik Traeholt Andersen <frederikhta@gmail.com>
2025-11-15 14:55:33 +0100

    init setup

--- GodmorgenBotFSharp/Program.fs
+++ GodmorgenBotFSharp/Program.fs
@@ -1,11 +1,39 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
+﻿open System.Threading.Tasks
+open GodmorgenBotFSharp
+open Microsoft.Extensions.Configuration
+open Microsoft.Extensions.DependencyInjection
 open Microsoft.Extensions.Hosting
+open NetCord
+open NetCord.Gateway
 open NetCord.Hosting.Gateway
+open NetCord.Hosting.Services.ApplicationCommands
 
-let builder = Host.CreateApplicationBuilder ()
-
-builder.Services.AddDiscordGateway () |> ignore
+let builder =
+    Host
+        .CreateDefaultBuilder()
+        .ConfigureAppConfiguration(fun context config -> config.AddJsonFile ("local.settings.json", optional = false, reloadOnChange = true) |> ignore)
+        .ConfigureServices (fun hostBuilderContext serviceCollection ->
+            serviceCollection.AddLogging() |> ignore
+            serviceCollection.AddHostedService<BackgroundTaskTest.BackgroundWorker>() |> ignore
+            serviceCollection.AddDiscordGateway () |> ignore
+        )
 
 let host = builder.Build ()
+
+let gatewayClient = host.Services.GetRequiredService<GatewayClient> ()
+
+gatewayClient.add_MessageCreate (fun message ->
+    task {
+        if message.Author.IsBot then
+            return ()
+        else
+            let! _ = message.ReplyAsync $"Hello, {message.Author.Username}!"
+            return ()
+    } |> ValueTask
+)
+
+type Delegate = delegate of User -> string
+let delegateFunc = Delegate (fun user -> $"Pong! <@{user.Id}>")
+host.AddSlashCommand("ping", "Replies with Pong!", delegateFunc) |> ignore
 
 host.RunAsync () |> Async.AwaitTask |> Async.RunSynchronously
